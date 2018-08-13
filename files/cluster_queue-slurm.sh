aa780c31c91fbe63e54e367e8b00b220d925e811
Helena Rasche <hxr@hx42.org>
2018-08-13 13:59:55 +0200

    forgot print

--- files/cluster_queue-slurm.sh
+++ files/cluster_queue-slurm.sh
@@ -1,4 +1,4 @@
 squeue | \
-	awk '(NR>1){ $2" "$4" "$5}' | \ # partition user state
+	awk '(NR>1){ print $2" "$4" "$5}' | \
 	uniq -c | \
 	awk '{print "cluster.queue,engine=slurm,state="$4",queue="$2",owner="$3" count="$1}'
